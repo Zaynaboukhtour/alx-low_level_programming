ecaec2999d636bacfcce370130f27fb9d6ca9012
Zaynaboukhtour <oukhtour.zaynab@gmail.com>
2023-06-16 03:08:59 -0700

    r

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,39 +7,39 @@
  */
 int main(void)
 {
-  int i, e, g;
+int i, e, g;
 
-  i = 48;
-  e = 48;
-  g = 48;
+i = 48;
+e = 48;
+g = 48;
 
-  while (e < 58)
-    {
-      i = 48;
-      while (i < 58)
-	{
-	  g = 48;
-	  while (g < 58)
-	    {
-	      if (e != i && e != g && i != g && e < i && i < g)
-		{
-		  putchar(e);
-		  putchar(i);
-		  putchar(g);
-		  if (i == 56 && e == 55 && g == 57)
-		    {
-		      break;
-		    }
-		  putchar(',');
-		  putchar(' ');
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+g = 48;
+while (g < 58)
+{
+if (e != i && e != g && i != g && e < i && i < g)
+{
+putchar(e);
+putchar(i);
+putchar(g);
+if (i == 56 && e == 55 && g == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
 		}
-	      g++;
-	    }
-	  i++;
-	}
-      e++;
-    }
-  putchar('\n');
-  return (0);
+g++;
+}
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
 }
 	 
